644cd931591d4cb868e17c5b73fc19a84dcd92e8
josephsutlive <josephs7@vt.edu>
2021-08-25 00:29:41 -0400

    Added osmosis force

---
+++ src/Physics/Forces/Osmosis.java
@@ -0,0 +1,15 @@
+package Physics.Forces;
+
+import Physics.Bodies.PhysicsBody;
+
+public class Osmosis extends Force{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void calculateForce(PhysicsBody body) {
+
+    }
+}
