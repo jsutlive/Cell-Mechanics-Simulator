ba26343ad7d7587615975998af21961461b95131
josephsutlive <josephs7@vt.edu>
2021-08-25 09:20:05 -0400

    small changes to cell group, preparing for larger refactor

--- src/Physics/Bodies/Cell/CellGroup.java
+++ src/Physics/Bodies/Cell/CellGroup.java
@@ -1,18 +1,27 @@
 package Physics.Bodies.Cell;
 
+import Physics.Forces.Force;
+
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 public class CellGroup
 {
     private List<Cell> cells = new ArrayList<Cell>();
     private Color color;
+    private Collection<Force> forces = new HashSet<>();
 
     public void addCell(Cell cell)
     {
         if(color != null) cell.setColor(color);
         cells.add(cell);
+    }
+
+    public void attachForce(Force force) {
+        forces.add(force);
     }
 
     public List<Cell> getCells()
