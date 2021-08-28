5ef1997ea44586d7e638e49cbf56421e9623d71d
josephsutlive <josephs7@vt.edu>
2021-08-28 18:44:56 -0400

    Added tests folder to begin implementing tests

---
+++ test/Model/DrosophilaEmbryoTest.java
@@ -0,0 +1,18 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DrosophilaEmbryoTest {
+
+    @Test
+    void getAllCells()
+    {
+
+    }
+
+    @Test
+    void generateOrganism() {
+    }
+}
