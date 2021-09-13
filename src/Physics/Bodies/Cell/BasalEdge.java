a0a22ee33e410418959acac53d1f35258b6f16da
josephsutlive <josephs7@vt.edu>
2021-09-13 14:32:31 -0400

    add more tests

--- src/Physics/Bodies/Cell/BasalEdge.java
+++
@@ -1,9 +0,0 @@
-package Physics.Bodies.Cell;
-
-import Physics.Bodies.Vertex;
-
-public class BasalEdge extends CellEdge {
-    public BasalEdge(Vertex a, Vertex b) {
-        super(a, b);
-    }
-}
--- src/Physics/Bodies/Cell/LateralEdge.java
+++
@@ -1,9 +0,0 @@
-package Physics.Bodies.Cell;
-
-import Physics.Bodies.Vertex;
-
-public class LateralEdge extends CellEdge{
-    public LateralEdge(Vertex a, Vertex b) {
-        super(a, b);
-    }
-}
