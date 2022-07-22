8dd3a0d083e686d2c274a9daea3da4820bdda01b
jsutlive <jsutlive@gmail.com>
2022-07-22 15:07:47 -0400

    Refactored Renderer to make it easier to implement new Renderer types

--- src/Engine/Renderer.java
+++ src/Engine/Renderer.java
@@ -1,25 +1,19 @@
 package Engine;
 
-import Engine.States.State;
-import GUI.DisplayWindow;
 import Utilities.Geometry.Vector2i;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.awt.*;
-import java.awt.image.BufferStrategy;
 
-public class Renderer implements Runnable
-{
-    private static Renderer instance;
+public abstract class Renderer implements Runnable{
+    //Graphics object that our painter class references to draw objects
+    protected static Graphics g;
 
-    /**
-     * Graphics object that our painter class references to draw objects
-     */
-    private static Graphics g;
+    //Default color by which to paint objects in a scene
     public static final Color defaultColor = Color.white;
-    DisplayWindow displayWindow;
-    BufferStrategy bufferStrategy;
-    String title;
-    int width, height;
+
+    //Renderer object singleton instance.
+    private static Renderer instance;
 
     /**
      * Used to generate a singleton instance of our Renderer.
@@ -29,68 +23,34 @@ public class Renderer implements Runnable
     {
         if(instance == null)
         {
-            instance = new Renderer();
+            // by default, we load the simpleRenderer
+            // change the Renderer type here, preferably using the same "makeInstance" convention.
+
+            /*
+                Example "makeInstance function:
+
+                 public static Renderer makeInstance(){
+                    if (getInstance()!= null) return getInstance();         // maintain singleton renderer
+                    return new ExampleRenderer();                           // create the renderer instance
+                }
+    }
+             */
+            instance = SimpleRenderer.makeInstance();
         }
         return instance;
     }
-    private Renderer()
-    {
-        width = Simulation.bounds.x;
-        height = Simulation.bounds.y;
-        title = Simulation.title;
-        displayWindow = new DisplayWindow(title, width, height);
-    }
 
-    /**
-     * Renders graphics to the screen. Should only be accessed from the Simulation object.
-     */
     @Override
-    public void run()
-    {
-        bufferStrategy = displayWindow.GetCanvas().getBufferStrategy();
-        if(bufferStrategy == null)
-        {
-            displayWindow.GetCanvas().createBufferStrategy(3);
-            return;
-        }
-        g = bufferStrategy.getDrawGraphics();
-
-        g.clearRect(0,0,width,height);
-
-        try {
-            if(State.GetState() != null)
-            {
-                State.GetState().Render();
-
-            }
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-        bufferStrategy.show();
-        g.dispose();
-
+    // run the renderer in an update loop
+    public void run() {
 
     }
+    // Draw any circular objects or points
+    public abstract void drawCircle(Vector2i center, int radius);
 
-    public DisplayWindow GetDisplayWindow()
-    {
-        return displayWindow;
-    }
+    // Draw any lines
+    public abstract void drawLine(Vector2i pointA, Vector2i pointB);
 
-    public void setColor(Color color){
-        if(g.getColor()==color) return;
-        g.setColor(color);
-    }
-
-    public void drawCircle(Vector2i center, int radius){
-        g.drawOval(center.x - radius/2, center.y - radius/2, radius, radius);
-    }
-
-    public void drawLine(Vector2i pointA, Vector2i pointB){
-        g.drawLine(pointA.x, pointA.y, pointB.x, pointB.y);
-    }
-
+    // Set the renderer color
+    public abstract  void setColor(Color color);
 }
---
+++ src/Engine/SimpleRenderer.java
@@ -0,0 +1,83 @@
+package Engine;
+
+import Engine.States.State;
+import GUI.DisplayWindow;
+import Utilities.Geometry.Vector2i;
+
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class SimpleRenderer extends Renderer
+{
+    DisplayWindow displayWindow;
+    BufferStrategy bufferStrategy;
+    String title;
+    int width, height;
+
+    public static Renderer makeInstance(){
+        // prevent creation of multiple renderer instances
+        if (getInstance()!= null) return getInstance();
+        return new SimpleRenderer();
+    }
+
+    private SimpleRenderer()
+    {
+        width = Simulation.bounds.x;
+        height = Simulation.bounds.y;
+        title = Simulation.title;
+        displayWindow = new DisplayWindow(title, width, height);
+    }
+
+    /**
+     * Renders graphics to the screen. Should only be accessed from the Simulation object.
+     */
+    @Override
+    public void run()
+    {
+        bufferStrategy = displayWindow.GetCanvas().getBufferStrategy();
+        if(bufferStrategy == null)
+        {
+            displayWindow.GetCanvas().createBufferStrategy(3);
+            return;
+        }
+        g = bufferStrategy.getDrawGraphics();
+
+        g.clearRect(0,0,width,height);
+
+        try {
+            if(State.GetState() != null)
+            {
+                State.GetState().Render();
+
+            }
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        bufferStrategy.show();
+        g.dispose();
+
+
+    }
+
+    public DisplayWindow GetDisplayWindow()
+    {
+        return displayWindow;
+    }
+
+    public void setColor(Color color){
+        if(g.getColor()==color) return;
+        g.setColor(color);
+    }
+
+    public void drawCircle(Vector2i center, int radius){
+        g.drawOval(center.x - radius/2, center.y - radius/2, radius, radius);
+    }
+
+    public void drawLine(Vector2i pointA, Vector2i pointB){
+        g.drawLine(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+
+}
