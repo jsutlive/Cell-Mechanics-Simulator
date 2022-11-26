54b73eff2e084c821717b94d3983cc3e08b4ce4e
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-11-25 19:56:17 -0500

    try drawing some points on canvas

--- GUI/index.js
+++ GUI/index.js
@@ -1,2 +1,11 @@
-let fs = require("fs")
-console.log(fs.readdirSync("./"))
+const { spawn } = require('node:child_process');
+const java = spawn('java', ['-jar', './MorphogenesisSimulationV2.jar']);
+
+const canvas = document.querySelector("canvas")
+const ctx = canvas.getContext("2d")
+ctx.fillStyle="rgb(0,0,0)"
+
+java.stdout.on('data', (data) => {
+    parsed = data.toString().split(" ").flatMap(e=>parseInt(e))
+    ctx.fillRect(parsed[0],parsed[1],2,2)
+});
