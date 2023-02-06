9654e50dc0f82b8ab229ba673bcd84c6697cd16f
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2023-02-05 21:11:01 -0500

    in mesh stiffness, move surround 2 cells in the opposite direction as well

--- src/Morphogenesis/Physics/Collision/MeshStiffness2D.java
+++ src/Morphogenesis/Physics/Collision/MeshStiffness2D.java
@@ -16,14 +16,14 @@ import static Utilities.Geometry.Geometry.calculateAngleBetweenPoints;
 public class MeshStiffness2D extends Force {
 
     private HashMap<ArrayList<Node2D>, Float> edgeAngleHashMap = new HashMap<>();
-    public float constant = 3.5f;
-    public float cornerFactor = 2f;
+    public float constant = 1.5f;
+    public float cornerFactor = 0.5f;
 
     @Override
     public void awake() {
         List<Node2D> nodes = getComponent(Mesh.class).nodes;
         int size = nodes.size();
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < size-1; i++){
             int a, b, c;
             if(i == 0){
                 a = size-1; b = i; c = i + 1;
@@ -41,6 +41,11 @@ public class MeshStiffness2D extends Force {
     @Override
     public void update() {
         for(ArrayList<Node2D> key : edgeAngleHashMap.keySet()){
+            Vector2f p1 = key.get(0).getPosition();
+            Vector2f p2 = key.get(1).getPosition();
+            Vector2f p3 = key.get(2).getPosition();
+            Vector normal1 = CustomMath.normal(p1,p2);
+            Vector normal2 = CustomMath.normal(p2,p3);
             Vector normal = getNormalForNodeSet(key);
             float theta = calculateAngleBetweenNodes(key);
             float restingTheta = edgeAngleHashMap.get(key);
@@ -48,8 +53,12 @@ public class MeshStiffness2D extends Force {
                 float distFromTheta = theta - restingTheta;
                 if (restingTheta > 130 || restingTheta < 45) {
                     addForceToBody(key.get(1), normal.mul(constant * distFromTheta));
+                    addForceToBody(key.get(0), normal1.mul(constant * distFromTheta).neg());
+                    addForceToBody(key.get(2), normal2.mul(constant * distFromTheta).neg());
                 }else{
                     addForceToBody(key.get(1), normal.mul(constant * cornerFactor * distFromTheta));
+                    addForceToBody(key.get(0), normal1.mul(constant * cornerFactor * distFromTheta).neg());
+                    addForceToBody(key.get(2), normal2.mul(constant * cornerFactor * distFromTheta).neg());
                 }
             }
         }
