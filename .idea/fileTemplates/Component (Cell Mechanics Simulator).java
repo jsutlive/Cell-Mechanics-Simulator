1a299b1deea25096fd3b1caf7691e080ed9218e0
jsutlive <jsutlive@gmail.com>
2023-05-02 00:09:22 -0400

    add component template

---
+++ .idea/fileTemplates/Component (Cell Mechanics Simulator).java
@@ -0,0 +1,16 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public class ${NAME} extends Component {
+
+    // Start is called once per frame
+    @Override
+    public void start(){
+    
+    }
+    
+    // Update is called once per physics step
+    @Override
+    public void update(){
+    
+    }
+}
